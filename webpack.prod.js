9a398d37021c128d373de4c5a51132de8c5e12f4
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2018-01-26 17:54:30 +0500

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules']
+      },
+      {
+        test: /\.svg$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [new UglifyJsPlugin()]
+};
